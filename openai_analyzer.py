1ae7589c93b7caa12fa9bceab8e3b1c412dd56b8
skjerdal <skjerdalthomas@gmail.com>
2024-10-24 11:52:13 +0200

    Added vercel

--- openai_analyzer.py
+++ openai_analyzer.py
@@ -115,4 +115,5 @@ def analyze_openai():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    port = int(os.getenv('PORT', 5001))
+    app.run(debug=True, port=port)
